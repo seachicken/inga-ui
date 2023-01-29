639868f44f62974e4d85f0ad7e6feaacededb972
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-01-29 09:36:02 +0900

    Add json path argument

--- render.js
+++ render.js
@@ -1,7 +1,9 @@
 import Mustache from 'mustache';
 import fs from 'fs';
 
+const jsonPath = process.argv[2] ? process.argv[2] : './data/report.json'
+const report = fs.readFileSync(jsonPath);
+
 const template = fs.readFileSync('./templates/index.html');
-const report = fs.readFileSync('./data/report.json');
 const html = Mustache.render(template.toString(), report);
 fs.writeFileSync('./index.html', html);
