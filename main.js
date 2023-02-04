555e4d2f0a50240714f1d99be07eebf19861139b
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-02-04 15:41:52 +0900

    Fix JSON structure

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { tw } from 'twind';
 import FileTree from './components/FileTree.js';
 import HierarchyView from './components/HierarchyView.js';
 
-const report = JSON.stringify(window['__inga__'].report);
+const report = JSON.stringify(window['__inga__']);
 
 window.customElements.define('file-tree', FileTree);
 window.customElements.define('hierarchy-view', HierarchyView);
