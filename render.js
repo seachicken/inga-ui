6dd543fa429ef4b0e84df655be96f1ff3a70cb18
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-02-10 21:56:19 +0900

    Remove mustache.js

--- render.js
+++ render.js
@@ -5,5 +5,5 @@ const jsonPath = process.argv[2] ? process.argv[2] : './data/report.json'
 const report = fs.readFileSync(jsonPath);
 
 const template = fs.readFileSync('./templates/index.html');
-const html = Mustache.render(template.toString(), report);
+const html = template.toString().replace('{{report}}', report);
 fs.writeFileSync('./index.html', html);
