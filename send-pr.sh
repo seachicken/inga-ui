57201b60b03a5a5abe287133a8dd76a577abebe3
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-06-12 20:46:17 +0900

    fix

--- send-pr.sh
+++ send-pr.sh
@@ -10,8 +10,14 @@ fi
 cd $INGA_WORK_PATH
 
 GITHUB_URL=$(gh repo view --json url --jq .url)
+echo "GITHUB_URL: $GITHUB_URL"
 HEAD_SHA=$(gh pr view --json headRefOid --jq .headRefOid)
+echo "HEAD_SHA: $HEAD_SHA"
+
+cd -
+
 PR_COMMENT=$(npm run --silent print-pr --ingapath=${INGA_WORK_PATH}/reports/report.json --ingaurl=${GITHUB_URL} --ingasha=${HEAD_SHA})
+echo "PR_COMMENT: $PR_COMMENT"
 
 gh pr comment $GITHUB_URL --body "$PR_COMMENT" --edit-last
 
