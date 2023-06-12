c073a3e1ac500c773a07600d15668b056424265a
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-06-12 20:30:20 +0900

    fix

--- send-pr.sh
+++ send-pr.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -e
 
-JSON_PATH="${1-./reports/report.json}"
+INGA_WORK_PATH="${1-.}"
 
 if ! type -p gh >/dev/null; then
   echo "gh not found on the system" >&2
   exit 1
 fi
 
+cd $INGA_WORK_PATH
+
 GITHUB_URL=$(gh repo view --json url --jq .url)
 HEAD_SHA=$(gh pr view --json headRefOid --jq .headRefOid)
-PR_COMMENT=$(npm run --silent print-pr --ingapath=${JSON_PATH} --ingaurl=${GITHUB_URL} --ingasha=${HEAD_SHA})
+PR_COMMENT=$(npm run --silent print-pr --ingapath=${INGA_WORK_PATH}/reports/report.json --ingaurl=${GITHUB_URL} --ingasha=${HEAD_SHA})
 
 gh pr comment $GITHUB_URL --body "$PR_COMMENT" --edit-last
 
