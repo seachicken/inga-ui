2b5ccbe1dac030fd9db2c1c3068629c7851fbbc0
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-06-12 20:50:47 +0900

    fix

--- send-pr.sh
+++ send-pr.sh
@@ -7,17 +7,14 @@ if ! type -p gh >/dev/null; then
   exit 1
 fi
 
-cd $INGA_WORK_PATH
-
-GITHUB_URL=$(gh repo view --json url --jq .url)
+GITHUB_URL=$(cd $INGA_WORK_PATH && gh repo view --json url --jq .url)
 echo "GITHUB_URL: $GITHUB_URL"
-HEAD_SHA=$(gh pr view --json headRefOid --jq .headRefOid)
+HEAD_SHA=$(cd $INGA_WORK_PATH && gh pr view --json headRefOid --jq .headRefOid)
 echo "HEAD_SHA: $HEAD_SHA"
 
-cd -
-
 PR_COMMENT=$(npm run --silent print-pr --ingapath=${INGA_WORK_PATH}/reports/report.json --ingaurl=${GITHUB_URL} --ingasha=${HEAD_SHA})
-echo "PR_COMMENT: $PR_COMMENT"
+
+cd $INGA_WORK_PATH
 
 gh pr comment $GITHUB_URL --body "$PR_COMMENT" --edit-last
 
