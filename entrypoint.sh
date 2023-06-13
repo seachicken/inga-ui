e8c14fb2d477b61d84cb95301183fe6f3fad32e7
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-06-13 16:47:41 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 COMMAND=$1
 INGA_WORK_PATH="${2-.}"
+HOME_DIR="${3-/inga-ui}"
 
 if ! type -p gh >/dev/null; then
   echo "gh not found on the system" >&2
@@ -14,11 +15,12 @@ HEAD_SHA=$(cd $INGA_WORK_PATH && gh pr view --json headRefOid --jq .headRefOid)
 echo "HEAD_SHA: $HEAD_SHA"
 
 if [ $1 = "pr-report" ]; then
-  PR_COMMENT=$(npm run --silent print-pr --ingapath=${INGA_WORK_PATH}/reports/report.json --ingaurl=${GITHUB_URL} --ingasha=${HEAD_SHA})
+  PR_COMMENT=$(cd $HOME_DIR && npm run --silent print-pr --ingapath=${INGA_WORK_PATH}/reports/report.json --ingaurl=${GITHUB_URL} --ingasha=${HEAD_SHA})
   echo "PR_COMMENT: $PR_COMMENT"
   cd $INGA_WORK_PATH
   gh pr comment --body "$PR_COMMENT" --edit-last || gh pr comment --body "$PR_COMMENT"
 elif [ $1 = "html-report" ]; then
+  cd $HOME_DIR
   npm run build --ingapath=${INGA_WORK_PATH}/reports/report.json --ingaurl=${GITHUB_URL} --ingasha=${HEAD_SHA} -- --base=${GITHUB_URL}
 else
   echo "command is required" >&2
